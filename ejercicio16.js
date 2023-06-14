85e566968af3da467e6924eab2e473c3a85e8e85
martina1214 <136590831+martina1214@users.noreply.github.com>
2023-06-14 09:03:10 -0300

    Add files via upload

---
+++ ejercicio16.js
@@ -0,0 +1,20 @@
+function rango(comienzo, final, sumador) {
+    let numeros = [];
+      
+    for (let i = comienzo; i <= final; i += sumador) {
+        numeros.
+       
+    push(i);
+      }
+      
+      
+      }
+      
+    
+    return numeros;
+      
+    let comienzo = 1;
+let final = 10;
+let sumador = 2;
+let resultado = rango(comienzo, final, sumador);
+  
