97d02cb6737bb294e60d22178fdebd806b69244d
martina1214 <136590831+martina1214@users.noreply.github.com>
2023-06-14 09:04:33 -0300

    Add files via upload

---
+++ ejercicio20.js
@@ -0,0 +1,21 @@
+function sumattion(numero) {
+     let resultado = 0;
+
+    for (let i = 1; i <= numero; i++) {
+        resultado += i;
+      }
+        resultado += i;
+      }
+        resultado += i;
+      
+        resultado += i;
+      
+        resultado += i;
+     
+        resultado += i;
+    
+    return resultado;
+
+    let numero = 5;
+let resultado = sumattion(numero);
+    
