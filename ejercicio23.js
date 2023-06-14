221c4c80b8eacc7545757f8f779c7b56424b81bd
martina1214 <136590831+martina1214@users.noreply.github.com>
2023-06-14 09:05:30 -0300

    Add files via upload

---
+++ ejercicio23.js
@@ -0,0 +1,16 @@
+function arregloDeObjetos(numero) {
+    let arreglo = [];
+    for (let i = 1; i <= numero; i++) {
+        
+    let objeto = { valor: i };
+        arreglo.
+       
+    push(objeto);
+      }
+    
+      }
+
+    return arreglo;
+
+    let numero = 5;
+let resultado = arregloDeObjetos(numero);
