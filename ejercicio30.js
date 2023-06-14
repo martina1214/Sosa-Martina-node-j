942452a50be3774c76660ccdb9fcd80168600f79
martina1214 <136590831+martina1214@users.noreply.github.com>
2023-06-14 09:08:49 -0300

    Add files via upload

---
+++ ejercicio30.js
@@ -0,0 +1,20 @@
+let total = 0;
+let input = parseInt(prompt("Teclea un número:"));
+
+while(input !== 0) {
+  total += input;
+  input = 
+
+  total += input;
+  input =
+
+  total += input;
+  input
+
+  total += input; 
+  total += input;
+ 
+parseInt(prompt("Teclea otro número o 0 para salir:"));
+}
+
+console.log("La suma total es: " + total);
