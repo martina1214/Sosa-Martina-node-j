91bc31f44bfe7da5ebbbbbbabe75a5b2448a35e2
martina1214 <136590831+martina1214@users.noreply.github.com>
2023-06-14 09:07:18 -0300

    Add files via upload

---
+++ ejercicio27.js
@@ -0,0 +1,12 @@
+function gananciaTotal(balances) {
+    let total = 0;
+    for (let i = 0; i < balances.length; i++) {
+        total += balances[i];
+      }
+        total += balances[i];
+      }
+        total += balances[i];
+        total += balances[i];
+        total += balances[i];
+     
+    return total;
