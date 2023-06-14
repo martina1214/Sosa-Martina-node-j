b8a14a82d6770780caa0896e92573b5915cc6805
martina1214 <136590831+martina1214@users.noreply.github.com>
2023-06-14 09:03:34 -0300

    Add files via upload

---
+++ ejercicio17.js
@@ -0,0 +1,16 @@
+function nuevoArreglo(numero) {
+    let arreglo = [];
+     
+    for (let i = 0; i < numero; i++) {
+        arreglo.
+       
+    push(i);
+      }
+      
+      
+      }
+      
+    return arreglo;
+
+    let numero = 5;
+let resultado = nuevoArreglo(numero);
