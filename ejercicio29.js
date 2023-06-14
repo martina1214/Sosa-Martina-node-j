3e30e38fbf5a45d83b908f82a10f08904bfceace
martina1214 <136590831+martina1214@users.noreply.github.com>
2023-06-14 09:08:06 -0300

    Add files via upload

---
+++ ejercicio29.js
@@ -0,0 +1,18 @@
+function contarHasta(num) {
+      let i = 0;
+    const listaNumeros = [];
+    
+    while (i <= num) {
+        listaNumeros.
+        listaNumeros
+    
+    push(i);
+        i++;
+      }
+        i++;
+      }
+        i++;
+        i++;
+    
+        i++;
+    return listaNumeros;
