9753f622279ff7be031ec143c09121c1d5d14a38
martina1214 <136590831+martina1214@users.noreply.github.com>
2023-06-14 09:03:58 -0300

    Add files via upload

---
+++ ejercicio18.js
@@ -0,0 +1,29 @@
+function breakStatement(numero) {
+    let arreglo = [];
+    let suma = 0;
+     
+    for (let i = 0; i < 10; i++) {
+        numero += 
+       
+    2;
+        arreglo.
+       
+    push(numero);
+    
+        suma += numero;
+        suma += numero;
+        suma += numero;
+        
+    if (suma === (i + 1) * numero) {
+  
+    return "Se interrumpió la ejecución";
+    break;
+        }
+      }
+      
+      
+        }
+    return arreglo;
+
+    let numero = 1;
+    let resultado = breakStatement(numero);
